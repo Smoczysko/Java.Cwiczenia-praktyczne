7f03e9588da03aa95d6c3d8e14dd19d343c6bcd1
Lukasz Rybka <darkstariw@gmail.com>
2009-07-23 23:10:29 +0200

    Poprawione znaki polskie (likwidacja tymczasowa)

--- Rozdzial_02/Zadanie_2.28.java
+++ Rozdzial_02/Zadanie_2.28.java
@@ -1,6 +1,6 @@
 /* Niniejszy kod pochodzi z ksiazki "Java. Cwiczenia praktyczne" Wydanie II autora Marcina Lisa
 *  Jest on dolaczony do ww. ksiazki w postaci elektronicznej pod adresem ftp://ftp.helion.pl/przyklady/cwjav2.zip
-*  Komentarze jak i kod programu sa przedmiotem artykulow na lamach bloga £ukasza Rybki - Akademia Javy
+*  Komentarze jak i kod programu sa przedmiotem artykulow na lamach bloga Lukasza Rybki - Akademia Javy
 *  Blog jest dostepny pod adresem: http://akademiajavy.blogspot.com
 */
 
